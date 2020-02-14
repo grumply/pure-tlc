7bdc960f044f9683396aa030e2c6eaa56dca26d5
Sean Hickman <sean@grumply.com>
2020-02-13 23:37:12 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, stdenv }:
 mkDerivation {
   pname = "pure-tlc";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base ];
   homepage = "github.com/grumply/pure-tlc";
